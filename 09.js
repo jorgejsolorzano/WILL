940edeaf15b495a21e7b1730a9cf49c36589b5ce
jorgejsolorzano <jorgej.solorzano@gmail.com>
2023-02-13 11:42:48 -0500

    Desarrollando el taller simulacion (09.js)  el dia 13-02-2023

--- 09.js
+++ 09.js
@@ -24,9 +24,20 @@ function filtrar(funcion) {
   // productos.filtrar(function(p) {
   //   return p.price >= 50;
   // }) => [{price: 100, name:'tv'}]
-
+  Array.prototype.filtrar = function(cb){
+    let filtrados = [];
+    for(i=0; i < this.length; i++){
+      if(cb(this[i])){
+        filtrados.push(this[i]); 
+      }
+    }
+    return filtrados;
+  };
 };
 
 // No modifiques nada debajo de esta linea //
 
 module.exports = filtrar
+
+
+
